9bbf176a733cedaebfca57a505235dc049bd767e
Pulipull <syaifularifin076@gmail.com>
2026-06-11 14:26:35 -0700

    fix

--- public/index.php
+++ public/index.php
@@ -14,4 +14,18 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+// ========== TAMBAHKAN INI UNTUK VERCEL ==========
+$app->useStoragePath('/tmp');
+$_ENV['SESSION_DRIVER'] = 'array';
+$_ENV['CACHE_STORE'] = 'array';
+$_ENV['LOG_CHANNEL'] = 'stderr';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['QUEUE_CONNECTION'] = 'sync';
+putenv('SESSION_DRIVER=array');
+putenv('CACHE_STORE=array');
+putenv('LOG_CHANNEL=stderr');
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('QUEUE_CONNECTION=sync');
+// ================================================
+
 $app->handleRequest(Request::capture());
