1c8f597fe2c7c1a5f8added9857f068c1c1dfba7
Pulipull <syaifularifin076@gmail.com>
2026-06-11 10:27:56 -0700

    Fix Deploy

--- api/index.php
+++ api/index.php
@@ -1,2 +1,13 @@
 <?php
+
+// Paksa bersihkan config cache biar Vercel baca .env yang baru
+if (isset($_GET['clear-cache'])) {
+    require __DIR__ . '/../bootstrap/app.php';
+    echo "Memperbarui konfigurasi...<br>";
+    Illuminate\Support\Facades\Artisan::call('config:clear');
+    Illuminate\Support\Facades\Artisan::call('view:clear');
+    Illuminate\Support\Facades\Artisan::call('route:clear');
+    die("Cache Laravel berhasil dibersihkan! Silakan buka halaman utama tanpa tambahan teks di URL.");
+}
+
 require __DIR__ . '/../public/index.php';
